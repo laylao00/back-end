ee8e221b1b89429c892e4e6089a1f108d3c977ca
laylao00 <santos.laylon@escola.pr.gov.br>
2026-03-12 09:01:50 -0300

    Create .110326.py

---
+++ conteudo/.110326.py
@@ -0,0 +1,4 @@
+##OPERADORES ARITMÉTICOS
+#Os operadores aritméticos são binários ou seja precisa de dois operandos para trabalhar.
+#Os operandos são os números que ficam antes e depois do operador aritmético.
+#Para a verificação da igualdade entre dois elementos em Python utiliza-se 2 símbolos de igual.
