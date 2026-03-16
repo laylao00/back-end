397ed5c962da9d9bd2319317f9c73eae754a6626
laylao00 <santos.laylon@escola.pr.gov.br>
2026-03-16 10:43:06 -0300

    Create cont005.py

---
+++ conteudo/cont005.py
@@ -0,0 +1,19 @@
+##ORDEM DE PRECEDÊNCIA
+# Definição: é a ordem na qual os operadores serão executados
+# 1 Expressões dentro de parenteses
+# 2 Em seguida vem as pontenciações
+# 3 Em terceiro lugar vem a multiplicação, a divisão normal, 
+# a divisão inteira e o resto da divisão. No caso de ter mais de um deles você resolve quem aparecer primeiro.
+# 4 E por último a soma e a subtração
+
+##EXEMPLO
+#5+3*2==
+#3*5+4**2==
+#3*(5+4)**2==
+# O Python é utilizado em cálculos científicos é o tamanho da memória ou seja conseguimos chegar a números extremamente grandes
+
+## OUTROS OPERADORES
+# Para exponenciação podemos usar o pow(x,y) onde o número x é elevado ao número y
+# Para raiz quadrada podemos usar o **(1/2), elevando o número a 1/2 nos obtemos a raiz quadrada. Para raiz cúbica **(1/3). E assim por diante.
+# Podemos usar os operadores em strings 
+# Ex: '='*5 vai ficar '====='
