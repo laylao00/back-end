70a3239908beaa83264391dfa35cee8b310d1efa
laylao00 <santos.laylon@escola.pr.gov.br>
2026-03-26 08:03:16 -0300

    Create ex015.py

---
+++ exercicios/ex015.py
@@ -0,0 +1,3 @@
+celsius = float(input('Digite a temperatura em °Celsius'))
+fahrenheit = ((celsius * 9/5) + 32)
+print('A temperatura em Celsius é,{:.2f} e a em Fahrenheit é {:.2f}.'.format(celsius,fahrenheit))
