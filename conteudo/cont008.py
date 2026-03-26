b2283cb72f8bc085858ffd50fc59f78f0327c307
laylao00 <santos.laylon@escola.pr.gov.br>
2026-03-26 08:37:28 -0300

    Delete conteudo/cont008.py

--- conteudo/cont008.py
+++
@@ -1,4 +0,0 @@
-##Biblíoteca MATH
-# Essa biblioteca é bem utilizada no Python e seu nome significa matemática.
-# Quando importada ela traz diversos recursos matemáticos como por exemplo a funcionalidade ceil que arredonda os números para cima 
-# ou a funcionalidade floor que realiza o arredondamento para baixo.
