72288af73d0e1c07d9a293e5ca42719281c9b9b3
laylao00 <santos.laylon@escola.pr.gov.br>
2026-03-30 10:32:21 -0300

    Update bibliotecarandom.py

--- conteudo/bibliotecarandom.py
+++ conteudo/bibliotecarandom.py
@@ -1,5 +1,5 @@
 import random
-num = random()#O computador vai gerar um número aleatório, entre 0 e 1
+num = random.random()#O computador vai gerar um número aleatório, entre 0 e 1
 num2 = random.randint(1,10)
 print(num)
 print(num2)
