42f265ffe236e348264bc210ad2dc1bda9687ee5
laylao00 <santos.laylon@escola.pr.gov.br>
2026-04-16 11:21:59 -0300

    Create respostasdaprova.py

---
+++ exercicios/respostasdaprova.py
@@ -0,0 +1,10 @@
+1)r; from math import cos,tan,sin,radians
+angulo = 45
+radianos = radians(angulo)
+seno = sin(radianos)
+coss = cos(radianos)
+tang = tan(radianos)
+
+print('O ângulo é {}, com seu seno sendo {}, seu cosseno é {} e sua tangente é {}.'.format(angulo,seno,coss,tang))
+
+2)r; 
