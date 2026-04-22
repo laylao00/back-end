cac770f6c99bc45264b045b0a6d9d599251292ef
laylao00 <santos.laylon@escola.pr.gov.br>
2026-04-22 11:36:23 -0300

    Update recuperacaodaprova.py

--- exercicios/recuperacaodaprova.py
+++ exercicios/recuperacaodaprova.py
@@ -11,3 +11,16 @@ A função Trunc ou Truncate, serve para cortar depois da vírgula, transformand
 A função Sqrt ou Square Root serve para calcular a raiz quadrada de um número, caso a pessoa que esteja codando não queira utilizar a fórmula ''x/0,5".
 
 E eles pertencem a biblioteca math.
+
+3)r;
+dias = int(input('Por quantos dias, o carro foi usado?'))
+km = float(input('Quantos km foram rodados?'))
+diaria = dias*47
+percurso = km*0.25
+custo = diaria+percurso 
+print('O total a pagar é de R${}'.format(custo))
+
+4)r;
+salario = float(input('Qual seu salário?'))
+aumento = salario + (salario * 12/100)
+print('O seu salário é {:.2f}, mas com o aumento ficou {:.2f}.'.format(salario,aumento))
