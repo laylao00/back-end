5b93e7f415adf8667edede354c51d863edf066e0
laylao00 <santos.laylon@escola.pr.gov.br>
2026-04-22 11:30:22 -0300

    Update recuperacaodaprova.py

--- exercicios/recuperacaodaprova.py
+++ exercicios/recuperacaodaprova.py
@@ -9,3 +9,5 @@ A função Ceil ou mais conhecido como Ceiling serve para arredondar um número 
 A função Trunc ou Truncate, serve para cortar depois da vírgula, transformando de um número real (float) para um número inteiro (int)
 
 A função Sqrt ou Square Root serve para calcular a raiz quadrada de um número, caso a pessoa que esteja codando não queira utilizar a fórmula ''x/0,5".
+
+E eles pertencem a biblioteca math.
