4f3a643ed13d6265b4d3060881042208fa16a317
laylao00 <santos.laylon@escola.pr.gov.br>
2026-04-22 11:28:42 -0300

    Update recuperacaodaprova.py

--- exercicios/recuperacaodaprova.py
+++ exercicios/recuperacaodaprova.py
@@ -4,3 +4,8 @@ numeroaleatorio = randint(1,25)
 print(numeroaleatorio)
 
 2)r;
+A função Ceil ou mais conhecido como Ceiling serve para arredondar um número decimal para o número inteiro maior ou mais próximo possível.
+
+A função Trunc ou Truncate, serve para cortar depois da vírgula, transformando de um número real (float) para um número inteiro (int)
+
+A função Sqrt ou Square Root serve para calcular a raiz quadrada de um número, caso a pessoa que esteja codando não queira utilizar a fórmula ''x/0,5".
