c0c50284e48772da856ffbfce18df5bc417ef091
laylao00 <santos.laylon@escola.pr.gov.br>
2026-04-23 11:30:42 -0300

    Create ex018.py

---
+++ exercicios/ex018.py
@@ -0,0 +1,4 @@
+ca = float(input('Digite o cateto adjacente'))
+co = float(input('Digite o cateto oposto'))
+h = (co**2 + ca**2)
+print('O cateto adjacente é {}, o oposto é {} e a hipotenusa é {}'.format(ca,co,h))
