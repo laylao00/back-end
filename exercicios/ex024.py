278f8f4cab8edeaabe8aabcb421c9b0e3cdd1b8a
laylao00 <santos.laylon@escola.pr.gov.br>
2026-05-07 08:25:39 -0300

    Create ex024.py

---
+++ exercicios/ex024.py
@@ -0,0 +1,6 @@
+cidade = str(input('Em que cidade você nasceu? ')).strip()
+comeca_com_santo = cidade[:5].lower() == 'santo'
+if comeca_com_santo == True:
+    print('Sim, o nome da sua cidade começa com "santo"!')
+else:
+    print('Não, o nome da sua cidade não começa com "santo".')
