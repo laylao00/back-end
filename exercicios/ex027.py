52967375f3ebb6344a5acad0eb2838966ea642e0
laylao00 <santos.laylon@escola.pr.gov.br>
2026-05-07 08:59:32 -0300

    Create ex027.py

---
+++ exercicios/ex027.py
@@ -0,0 +1,4 @@
+nomecompleto = str(input('Digite seu nome completo: '))
+nomes = nomecompleto.split()
+print('O seu primeiro nome é {}'.format(nomes[0]))
+print('O seu último nome é {}'.format(nomes[-1]))
