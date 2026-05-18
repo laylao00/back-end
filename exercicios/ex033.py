28f94e3b0f4e42103c4a9e97f4ce576ce5de18b9
laylao00 <santos.laylon@escola.pr.gov.br>
2026-05-18 10:56:09 -0300

    Update ex033.py

--- exercicios/ex033.py
+++ exercicios/ex033.py
@@ -5,7 +5,7 @@ num3 = int(input('Escolha um número'))
 maior = num1
 if num2>num1 and num2>num3:
     maior = num2
-if num3>num2 and num3>num2:
+if num3>num1 and num3>num2:
     maior = num3
 
 menor = num1
