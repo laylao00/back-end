a85135364982637846740422cdf60a73247ccd18
laylao00 <santos.laylon@escola.pr.gov.br>
2026-05-20 10:47:42 -0300

    Create condicaoaninhada.py

---
+++ conteudo/condicaoaninhada.py
@@ -0,0 +1,5 @@
+## CODIÇÕES ANINHADA
+# São condições colocado dentro de outras condições. Em condições aninhadas eu utilizo o se não se,
+# dentro do se. Em Python o se não se é representado por elif condição: 
+# A partir do primeiro elif para mais condições você vai colocando um elif para cada condição.
+# Sempre que for usar o primeiro elif tem que usar um if antes e o else se torna opcional.  
