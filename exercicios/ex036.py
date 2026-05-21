a95bf08e3873567e0208824d481ea87fc11f8ffa
laylao00 <santos.laylon@escola.pr.gov.br>
2026-05-21 12:14:36 -0300

    Update ex036.py

--- exercicios/ex036.py
+++ exercicios/ex036.py
@@ -1,21 +1,8 @@
-valor_casa = float(input('Qual o valor da casa? R$ '))
-salario = float(input('Qual o seu salário? R$ '))
-anos = int(input('Em quantos anos pretende pagar? '))
-
-meses = anos * 12
-prestacao = valor_casa / meses
-limite = salario * 0.30
-
-print(f'\nPara pagar uma casa de R$ {valor_casa:.2f} em {anos} anos,', end=' ')
-print(f'a prestação será de R$ {prestacao:.2f}.')
-
-if prestacao <= limite:
-    print('Empréstimo pode ser CONCEDIDO!')
-else:
-    print('Empréstimo NEGADO!')
-
-#----------------------------------------------------
-
-valorcasa = float(input('Qual o valor da casa em R$?'))
-salario = float(input('Qual o seu salário em R$?'))
-anos
+casa = float(input('Valor da casa: R$'))
+salario = float(input('Salário do comprador: R$'))
+financiamento = int(input('Quantos anos de financiamento?'))
+prestacao = casa/(financiamento*12)
+if prestacao <=salario*0.3:
+    print('Para pagar uma casa de {} em {} anos a prestação será de R${:.2f} é sufiiente. Empréstimo aprovado'.format(casa,financiamento,prestacao))
+elif prestacao > salario*0.3:
+    print('Para pagar uma casa de {} em {} anos a prestação será de R${:.2f} é muito pouco. Empréstimo negado.'.format(casa,financiamento,prestacao))
