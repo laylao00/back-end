b30c2389824dfaa948f3ce0bdd9291d25d765c36
laylao00 <santos.laylon@escola.pr.gov.br>
2026-06-25 09:27:18 -0300

    Create ex040.py

---
+++ exercicios/ex040.py
@@ -0,0 +1,12 @@
+print("-=-" * 10)
+print("Analisador de triângulos")
+print("-=-" * 10)
+a = float(input("Primeiro segmento"))
+b = float(input("Segundo segmento "))
+c = float(input("Terceiro segmento"))
+c: f a<b+c and b<a+c and c<a+b and a== b ==
+print("Os segmentos acima podem formar um triangulo Equilatero")
+lif a<b+c and b<a+c and c<a+b and a== b or a== c or b==c:
+print("Os segmentos acima podem formar um triangulo Isósceles")
+lif a<b+c and b<a+c and c<a+b and a!= b and a!= c and b!=c:
+print("Os segmentos acima podem formar um triangulo Escaleno")
