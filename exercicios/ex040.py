c95fb4a0edd6a4a2e4bdaa782d11d97d13e0473d
laylao00 <santos.laylon@escola.pr.gov.br>
2026-06-25 09:29:42 -0300

    Update ex040.py

--- exercicios/ex040.py
+++ exercicios/ex040.py
@@ -6,7 +6,9 @@ b = float(input("Segundo segmento "))
 c = float(input("Terceiro segmento"))
 c: f a<b+c and b<a+c and c<a+b and a== b ==
 print("Os segmentos acima podem formar um triangulo Equilatero")
-lif a<b+c and b<a+c and c<a+b and a== b or a== c or b==c:
-print("Os segmentos acima podem formar um triangulo Isósceles")
-lif a<b+c and b<a+c and c<a+b and a!= b and a!= c and b!=c:
-print("Os segmentos acima podem formar um triangulo Escaleno")
+elif a<b+c and b<a+c and c<a+b and a== b or a== c or b==c:
+  print("Os segmentos acima podem formar um triangulo Isósceles")
+elif a<b+c and b<a+c and c<a+b and a!= b and a!= c and b!=c:
+  print("Os segmentos acima podem formar um triangulo Escaleno")
+else:
+  print("Não é possível formar um triãngulo.")
