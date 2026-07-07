62fd83887ce36dc28d42b229ceab7799151df513
laylao00 <santos.laylon@escola.pr.gov.br>
2026-07-07 10:55:58 -0300

    Update ex048.py

--- exercicios/ex048.py
+++ exercicios/ex048.py
@@ -4,3 +4,10 @@ print('Os 10 primeiros termos da PA são: ')
 for i in range(10):
     trm = a1 + i * r
     print(trm, end=' ')
+
+primeiro = int(input('Primeiro termo:'))
+razao = int(input('Razão: '))
+decimo = primeiro + 10 * razao #formula de PA
+for c in range(primeiro, decimo, razao):
+    print(f'{c}', end=' -> ')
+print('Acabou')
