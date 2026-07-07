5bbd278ac901cdea8c39ab2c2345f8a62d748c1c
laylao00 <santos.laylon@escola.pr.gov.br>
2026-07-07 10:44:44 -0300

    Update ex047.py

--- exercicios/ex047.py
+++ exercicios/ex047.py
@@ -4,3 +4,15 @@ for i in range(1,7):
     if numero % 2 == 0:
         pares += numero
 print(f'A soma dos números pares digitados é: {pares}')
+
+__________________________________________
+
+
+soma = 0
+cont = 0
+for c in range (1,7):
+    num = int(input(f"digite o {c} valor "))
+    if num%2 ==0:
+        soma += num
+        cont = cont +1
+print(f"Você informou {cont} numeros pares e a soma dos numeros pares é {soma}")
