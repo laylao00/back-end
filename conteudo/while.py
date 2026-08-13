380fbae21306ae938f7a9c3df37a2292006c8edb
laylao00 <santos.laylon@escola.pr.gov.br>
2026-08-13 07:49:11 -0300

    Create while.py

---
+++ conteudo/while.py
@@ -0,0 +1,7 @@
+#Estrutura de repetição While
+#Esta estrutura é utilizada para quando não sabemos o número de repetições devem ser executadas. 
+#Nessa estrutura repetimos o laço enquanto uma condição é atendida, 
+#quando a condição imposta não é mais atendida o laço para. 
+#A sintaxe desse laço é a seguinte: while + condição: 
+#Dentro da estrutura while podemos utilizar diferentes 
+#condicionais para que o código siga caminhos diferentes ao longo de sua execução.
