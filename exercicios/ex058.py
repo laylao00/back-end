eb09d6693cc735a6ba4f6e7a020c712823e97c21
laylao00 <santos.laylon@escola.pr.gov.br>
2026-08-20 11:41:09 -0300

    Update ex058.py

--- exercicios/ex058.py
+++ exercicios/ex058.py
@@ -1,17 +1,11 @@
-a1 = int(input('Digite o primeiro termo da PA: '))
-r = int(input('Digite a razão da PA: '))
-print('Os 10 primeiros termos da PA são: ')
-i = 0
-while i < 10:
-    trm = a1 + i * r
-    print(trm, end=' ')
-    i += 1
-print('\n')
+print('Gerador de PA: ')
+print('-=- '*10)
 primeiro = int(input('Primeiro termo: '))
-razao = int(input('Razão: '))
-decimo = primeiro + 10 * razao
-c = primeiro
-while c < decimo:
-    print(f'{c}', end=' -> ')
-    c += razao
-print('Acabou')
+razao = int(input('Razão da PA: '))
+termo = primeiro
+cont = 1
+while cont<=10:
+    print(f'{termo} -> ',end='')
+    termo += razao
+    cont += 1
+print('Fim')
