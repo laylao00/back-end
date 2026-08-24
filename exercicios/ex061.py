fea61a5e06e0c1a3d331790c34e6fdd1d510b841
laylao00 <santos.laylon@escola.pr.gov.br>
2026-08-24 10:53:23 -0300

    Create ex061.py

---
+++ exercicios/ex061.py
@@ -0,0 +1,9 @@
+s = 0
+c = 0
+while True:
+    n = int(input('Digite um número: '))
+    if n == 999:
+        break
+    s += n
+    c += 1
+print(f'Você digitou {c} números e a soma deles foi {s}')
