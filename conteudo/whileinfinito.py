d4e1ae84f370d172dc4ea29d12c323277a804463
laylao00 <santos.laylon@escola.pr.gov.br>
2026-08-26 10:50:35 -0300

    Create whileinfinito.py

---
+++ conteudo/whileinfinito.py
@@ -0,0 +1,5 @@
+#WHILE INFINITO
+## Nesse laço trabalhamos com um comando de parada que desvia a execução do bloco de código para o final do laço. 
+# Esse comando é chamado de break.
+# Esse comando é executado em laços while que não possuem testes lógicos, onde escrevemos while true no loop,
+# Que sem o break será executado para sempre.
