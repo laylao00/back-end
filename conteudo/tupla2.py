3abe8ae99e40b007cc37e42f24d98e17c614560a
laylao00 <santos.laylon@escola.pr.gov.br>
2026-09-14 11:36:32 -0300

    Create tupla2.py

---
+++ conteudo/tupla2.py
@@ -0,0 +1,4 @@
+#Tuplas(variáveis compostas)
+#Dentro das tuplas podemos usar métodos de fatiamento, de length e o uso das variáveis de repetição. 
+#As tuplas são imutáveis em Python ou seja os valores armazenados nessas variáveis não podem ser alterados após definidos. 
+#As tuplas são definidas entre parênteses
